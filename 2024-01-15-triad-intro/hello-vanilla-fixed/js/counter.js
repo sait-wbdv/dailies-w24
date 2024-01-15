2d2f8bec1e13046c2fb74597fc192e14203a86b9
ELTemi <ruth_olukunle@yahoo.com>
2024-01-15 15:56:21 -0700

    add day 1 starter code fixed for html intro

--- 2024-01-15-triad-intro/hello-vanilla-fixed/js/counter.js
+++ 2024-01-15-triad-intro/hello-vanilla-fixed/js/counter.js
@@ -2,7 +2,6 @@ function setupCounter(element) {
   let counter = 0;
   const setCounter = (count) => {
     counter = count;
-    
     element.innerHTML = `count is ${counter}`;
   }
   element.addEventListener('click', () => setCounter(counter + 1))
